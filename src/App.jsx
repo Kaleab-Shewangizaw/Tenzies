053bc8f26983509ea83ddbdf7bf075cc266384f9
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2024-11-30 22:21:55 +0300

    tenzise init

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import Dice from "./components/Dice";
+import { useState } from "react";
+export default function App() {
+  function generateAllNewDice() {
+    // const newDice = []
+    // for(let i = 0; i < 10; i++){
+    //     const randNum = Math.floor(Math.random() * 6)
+    //     newDice.push(randNum)
+    // }
+    // return newDice
+    // ----------------------------------------------------------------
+    // return new Array(10)
+    //   .fill(0)
+    //   .map((val, index) => (
+    //     <Dice value={Math.ceil(Math.random() * 6)} key={index} />
+    //   ));
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  const diceElement = dice.map((num, index) => (
+    <Dice value={num} key={index} />
+  ));
+
+  // const dice = generateAllNewDice().map((die, index)=>{
+  //     return  <Dice value={die} key={index}/>})
+
+  const rolldice = () => {
+    setDice(generateAllNewDice());
+  };
+
+  return (
+    <>
+      <main>
+        <div className="container">
+          {diceElement} 
+        </div>
+        
+        <button onClick={rolldice}>Roll Dice</button>
+      </main>
+    </>
+  );
+}
---
+++ src/components/Dice.jsx
@@ -0,0 +1,3 @@
+export default function Dice(props) {
+  return <button>{props.value}</button>;
+}
